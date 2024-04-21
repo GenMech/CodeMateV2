4b11125e27a9cae30e2d9663af55f10db2cef806
Gitesh Pareek <pareekgitesh89@gmail.com>
2024-04-21 16:04:32 +0530

    feat: navbar complete

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -3,20 +3,57 @@
 import { ModeToggle } from "@/components/modeToggle";
 import { Button } from "@/components/ui/button";
 import { signIn, signOut, useSession } from "next-auth/react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { LogOutIcon } from "lucide-react";
+import Link from "next/link";
+
+function AccountDropdown() {
+  const session = useSession();
+  // const isLoggedIn = !!session.data;
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant={"link"}>
+          <Avatar className="mr-2">
+            <AvatarImage src={session.data?.user?.image ?? ""} />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          {session.data?.user?.name && `Welcome, ${session.data?.user?.name}`}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem className="cursor-pointer" onClick={() => signOut()}>
+          <LogOutIcon className="mr-2" /> Sign Out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
 
 export function Header() {
   const session = useSession();
 
   return (
-    <header>
-      <div>
-        {session.data ? (
-          <Button onClick={() => signOut()}>Sign Out</Button>
-        ) : (
-          <Button onClick={() => signIn("google")}>Sign In</Button>
-        )}
-        {session.data?.user?.name ? `Welcome, ${session.data?.user?.name}` : ``}
-        <ModeToggle />
+    <header className="bg-gray-100 dark:bg-gray-900 py-3 container mx-auto">
+      <div className="flex justify-between items-center">
+        <Link href="/">CodeMate</Link>
+        <div className="flex items-center gap-4">
+          {!session.data ? (
+            <Button variant={"outline"} onClick={() => signIn("google")}>
+              Sign In
+            </Button>
+          ) : (
+            <AccountDropdown />
+          )}
+          <ModeToggle />
+        </div>
       </div>
     </header>
   );
