497e8142949430dce0569955e3ddf8a1e66a21a8
Gitesh Pareek <pareekgitesh89@gmail.com>
2024-04-22 14:05:07 +0530

    fix: Driizzle Too many clients bu fix

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,26 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
+import type { PostgresJsDatabase } from "drizzle-orm/postgres-js";
 
 // for query purposes
 const queryClient = postgres(process.env.DATABASE_URL!);
-const db = drizzle(queryClient, { schema });
+
+// Extra caching because drizzel ORM makes new client everytime we reload the nextjs server, so to fix that
+declare global {
+  var db: PostgresJsDatabase<typeof schema> | undefined;
+}
+
+let db: PostgresJsDatabase<typeof schema>;
+
+if (process.env.NODE_ENV === "production") {
+  db = drizzle(queryClient, { schema });
+} else {
+  if (!global.db) {
+    global.db = drizzle(queryClient, { schema });
+  }
+
+  db = global.db;
+}
 
 export { db };
