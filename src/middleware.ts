4922c699b9eba02e8849d070139bc71aaa90f6ee
Gitesh Pareek <pareekgitesh89@gmail.com>
2024-05-07 23:24:57 +0530

    feat: middleware fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,5 @@
 export { default } from "next-auth/middleware";
 
-export const config = { matcher: ["/yourRooms", "/browse", "/editRoom"] };
+export const config = {
+  matcher: ["/yourRooms", "/browse", "/editRoom", "/createRoom", "/rooms"],
+};
